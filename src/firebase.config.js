20e4c5b2042a7191ab7aee4a41d4e851305bfe84
Jahanvi Dubey <dubeyjahanvi07@gmail.com>
2024-09-25 19:33:55 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,18 +1,18 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; // Added storage import
 
 const firebaseConfig = {
   apiKey: "AIzaSyDXIWRyL2nwYEWtCnoLA8NxLbSP6q9zq_4",
   authDomain: "cook-book-cc54a.firebaseapp.com",
   projectId: "cook-book-cc54a",
-  storageBucket: "cook-book-cc54a.appspot.com",
+  storageBucket: "cook-book-cc54a.appspot.com", // Ensure the storage bucket is correct
   messagingSenderId: "862368538990",
   appId: "1:862368538990:web:dd870409ae9f068214217f"
 };
 
-
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
+const storage = getStorage(app); // Initialize storage
 
-export { db }
+export { db, storage };
